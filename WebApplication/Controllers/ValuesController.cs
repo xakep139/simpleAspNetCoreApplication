b06300e00596ddb180bc39557b7eb1f44b51fc61
Nikita Balabaev <n.balabaev@2Gis.ru>
2017-06-21 15:12:51 +0700

    initial commit

---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/{api-version:apiVersion}/values")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5/some_version_id
+        [HttpGet("{id:int}/{versionId}")]
+        public string GetVersion(int id, string versionId)
+        {
+            return $"value from {id} with version {versionId}";
+        }
+
+        // GET api/values/5/versions
+        [HttpGet("{id:int}/versions")]
+        public string GetVersions(int id)
+        {
+            return $"value from {id} with all versions";
+        }
+    }
+}
---
+++ WebApplication/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using WebApplication.Swashbuckle;
+
+namespace WebApplication
+{
+    public sealed class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddVersionedApiExplorer()
+                .AddApiExplorer()
+                .AddAuthorization()
+                .AddJsonFormatters()
+                .AddCors();
+
+            services.AddApiVersioning(options => options.ReportApiVersions = true);
+
+            services.AddSwaggerGen(
+                x =>
+                {
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        x.SwaggerDoc(description.GroupName, new Info { Title = $"Values API {description.ApiVersion}", Version = description.ApiVersion.ToString() });
+                    }
+
+                    x.OperationFilter<ImplicitApiVersionParameter>();
+                });
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            IApplicationLifetime appLifetime)
+        {
+            loggerFactory.AddConsole(_configuration.GetSection("Logging"));
+            app.UseMvc();
+            app.UseApiVersioning();
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+
+                    options.DocExpansion("list");
+                    options.EnabledValidator();
+                    options.ShowRequestHeaders();
+                });
+        }
+    }
+}
